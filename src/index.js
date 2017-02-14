df2aeb2cc59457c11702de6e9262f3c30b340110
Justincadburywong <justincadburywong@gmail.com>
2017-02-14 10:46:44 -0800

    update index.js to use CommentBox components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import CommentBox from './CommentBox';
 
 ReactDOM.render(
-  <App />,
+  <CommentBox />,
   document.getElementById('root')
 );
