aa3662b1e6bdf91e3a0b7a06679e9d41935affea
Justincadburywong <justincadburywong@gmail.com>
2017-02-16 14:51:13 -0800

    allow form to handle buttons on submit

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import style from './style';
-class CommentForm extends Component {
-constructor(props) {
-  super(props);
-  this.state = { author: '', text: '' };
-  this.handleAuthorChange = this.handleAuthorChange.bind(this);
-  this.handleTextChange = this.handleTextChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { author: '', text: '' };
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   handleAuthorChange(e) {
     this.setState({ author: e.target.value });
   }
@@ -20,34 +20,32 @@ constructor(props) {
     let author = this.state.author.trim();
     let text = this.state.text.trim();
     if (!text || !author) {
-      return;
-    }
-    this.props.onCommentSubmit({ author: author, text: text });
-    this.setState({ author: '', text: '' });
-    console.log(`${this.state.author} said “${this.state.text}”`)
-    //we will be tying this into the POST method in a bit
+    return;
+  }
+  this.props.onCommentSubmit({ author: author, text: text });
+  this.setState({ author: '', text: '' });
   }
   render() {
     return (
       <form style={ style.commentForm } onSubmit={ this.handleSubmit }>
-        <input
-          type='text'
-          placeholder='Your name…'
-          style={ style.commentFormAuthor}
-          value={ this.state.author }
-          onChange={ this.handleAuthorChange } />
-        <input
-          type='text'
-          placeholder='Say something…'
-          style={ style.commentFormText}
-          value={ this.state.text }
-          onChange={ this.handleTextChange } />
-        <input
-          type='submit'
-          style={ style.commentFormPost }
-          value='Post' />
-        </form>
-      )
-    }
+      <input
+        type='text'
+        placeholder='Your name…'
+        style={ style.commentFormAuthor}
+        value={ this.state.author }
+        onChange={ this.handleAuthorChange } />
+      <input
+        type='text'
+        placeholder='Say something…'
+        style={ style.commentFormText}
+        value={ this.state.text }
+        onChange={ this.handleTextChange } />
+      <input
+        type='submit'
+        style={ style.commentFormPost }
+        value='Post'/>
+      </form>
+    )
+  }
 }
 export default CommentForm;
