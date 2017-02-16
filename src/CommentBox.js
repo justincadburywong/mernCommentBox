9ed17f073ea8b061a4f8ee9256653b0c3f044d30
Justincadburywong <justincadburywong@gmail.com>
2017-02-16 12:27:41 -0800

    add axios get functions and setInterval to check new comments

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from '../data';
 import style from './style';
+
 class CommentBox extends Component {
- constructor(props) {
- super(props);
- this.state = { data: [] };
- }
- render() {
- return (
- <div style={ style.commentBox }>
- <h2>Comments:</h2>
- <CommentList data={ DATA }/>
- <CommentForm />
- </div>
- )
- }
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+
+    loadCommentsFromServer(){
+      axios.get(this.props.url)
+      .then(res => {
+        this.setState({ data: res.data });
+      })
+    }
+    handleCommentSubmit(comment) {
+    //add POST request
+    }
+
+    componentDidMount(){
+      this.loadCommentsFromServer();
+      setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+    }
+    render() {
+      return (
+        <div style={ style.commentBox }>
+          <h2>Comments:</h2>
+          <CommentList data={ this.state.data }/>
+          <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
+        </div>
+    )
+  }
 }
 export default CommentBox;
